c015e94e98da36b7c3b9990bb3f635574d347e8d
Furkan Yavuz <furknyavuz@gmail.com>
2022-03-23 21:55:37 +0300

    hero init

---
+++ projects/hero/src/lib/hero.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeroComponent } from './component/hero/hero.component';
+
+@NgModule({
+  declarations: [
+    HeroComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeroComponent
+  ]
+})
+export class HeroModule { }
---
+++ projects/hero/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of hero
+ */
+
+export * from './lib/component/hero/hero.component';
+export * from './lib/model/hero/hero.model';
+export * from './lib/hero.module';
